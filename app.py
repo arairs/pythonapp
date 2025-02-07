72538f1cdd5eed85602e9ff427219c626e926a9f
Iara <iaraotako@gmail.com>
2025-02-06 21:50:09 -0300

    06_02

--- app.py
+++ app.py
@@ -1,8 +1,44 @@
-from flask import Flask
+from flask import Flask, request
+import psycopg2
 app = Flask(__name__)
 @app.route('/')
 def home():
     return "Olá, Flask"
 
+@app.route('/item', methods=['POST'])
+def post_item():
+    data = request.get_json()
+    sql = f"INSERT INTO todolist(item, status) VALUES('{data['item']}','{data['status']}')"
+    banco(sql)
+    return data
+
+def banco(sql):
+    resultado = ""
+    try:
+        conn = psycopg2.connect(
+            host=host,
+            port=port,
+            dbname=dbname,
+            user=user,
+            password=password
+
+        )
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        cursor.close()
+        conn.commit()
+        conn.close()
+
+    except psycopg2.Error as e:
+        print("Erro na conexão do banco de dados")
+
+host = "dpg-cuhv0bdds78s73drb2l0-a.oregon-postgres.render.com"
+port = "5432"
+dbname = "senaidb_rz5u"
+user = "senaidb_rz5u_user"
+password = "6XvtSHpgvIDOzA2eLygXStXpEKsvZBpd"
+
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(debug=True)
+
+
